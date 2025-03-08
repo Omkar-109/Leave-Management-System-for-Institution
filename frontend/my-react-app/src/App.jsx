0a4f1433847e14e1312036b4f1dd1da5fb8c16db
Omkar-109 <127653970+Omkar-109@users.noreply.github.com>
2025-03-08 16:29:44 +0530

    created react app

---
+++ frontend/my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>DAa</h1>
+        <ul>
+          <li>one</li>
+          <li>two</li>
+          <li>3</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
