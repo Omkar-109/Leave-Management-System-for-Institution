bcc25ec926c19aa8bd4fa531fc19bd9fc22a5107
Omkar-109 <127653970+Omkar-109@users.noreply.github.com>
2025-03-09 15:34:21 +0530

    file create

---
+++ frontend/react/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+<div>
+<h1>DAa</h1>
+<ul>
+    <li>one</li>
+    <li>two</li>
+</ul>
+</div>
+, document.getElementById("root"))
