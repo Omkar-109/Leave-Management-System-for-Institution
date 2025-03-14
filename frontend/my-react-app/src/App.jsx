7b5360a0aefc5c9443276b61d4f4b18607226c8c
Omkar-109 <127653970+Omkar-109@users.noreply.github.com>
2025-03-14 23:00:26 +0530

    Update App.jsx

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -5,14 +5,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>DAa</h1>
-        <ul>
-          <li>one</li>
-          <li>two</li>
-          <li>3</li>
-        </ul>
-      </div>
+      
+      
     </>
   )
 }
